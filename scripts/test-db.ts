84bf08cd5be1375faebe304ea0328d79852092d0
CapsurSAC <redes03.capsur@gmail.com>
2025-05-13 14:38:42 -0500

    version delete scripts

--- scripts/test-db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-async function main() {
-  const progress = await prisma.progress.findMany();
-  console.log("Progresos:", progress);
-}
-
-main().finally(() => prisma.$disconnect());
