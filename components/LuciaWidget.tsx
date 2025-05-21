8a3479064f97afe2bc0874b3964796c459e2db94
CapsurSAC <redes03.capsur@gmail.com>
2025-05-21 18:18:45 -0500

    versión backend cursos y lwidget june

--- components/LuciaWidget.tsx
+++ components/LuciaWidget.tsx
@@ -14,7 +14,7 @@ export default function LuciaWidget() {
       onClick={handleClick}
       className="fixed bottom-4 right-4 z-50 bg-purple-600 text-white p-3 rounded-full shadow-lg hover:bg-purple-700 transition"
     >
-      💬 Lucía
+      💬 Mentor June
     </button>
   );
 }
